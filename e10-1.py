4b8662c40aa78d046cbbca81e6a75f6d61aabefd
kyopalab <kyono@kyopalab.com>
2025-05-08 16:25:03 +0900

    fixed header

--- e10-1.py
+++ e10-1.py
@@ -1,3 +1,48 @@
+# -----------------------------------------------------------------------------
+# File:    e10-1.py
+# Project: FIT2 2025
+# Author:  Kyopan
+# Date:    2025-05-08
+#
+# Description:
+#   A mouse-controlled paddle game where multiple balls fall from the top
+#   at random angles. You catch balls to earn points, and the speed
+#   increases with each catch.
+#
+# Usage:
+#   pyxel run e10-1.py
+#
+# Controls:
+#   - Mouse: Move the paddle horizontally
+#   - Catch falling balls to increase score
+#
+# Dependencies:
+#   - pyxel (https://github.com/kitao/pyxel)
+#
+# License:
+#   MIT License
+#
+#   Copyright (c) 2025 Kyopan
+#
+#   Permission is hereby granted, free of charge, to any person obtaining a copy
+#   of this software and associated documentation files (the "Software"), to deal
+#   in the Software without restriction, including without limitation the rights
+#   to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+#   copies of the Software, and to permit persons to whom the Software is
+#   furnished to do so, subject to the following conditions:
+#
+#   The above copyright notice and this permission notice shall be included in
+#   all copies or substantial portions of the Software.
+#
+#   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+#   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+#   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+#   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+#   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+#   SOFTWARE.
+# -----------------------------------------------------------------------------
+
 import pyxel
 
 # Initialize a 200×200 window
--- e10-2.py
+++ e10-2.py
@@ -1,3 +1,47 @@
+# -----------------------------------------------------------------------------
+# File:    e10-2.py
+# Project: FIT2 2025
+# Author:  Kyopan
+# Date:    2025-05-08
+#
+# Description:
+#   Mouse-controlled paddle game with multiple falling balls.
+#   Catch balls to score points; missing 10 balls ends the game.
+#
+# Usage:
+#   pyxel run e10-2.py
+#
+# Controls:
+#   - Mouse: Move paddle horizontally
+#   - Catch balls: Earn points
+#   - Game ends after 10 missed balls
+#
+# Dependencies:
+#   - pyxel (https://github.com/kitao/pyxel)
+#
+# License:
+#   MIT License
+#
+#   Copyright (c) 2025 Kyopan
+#
+#   Permission is hereby granted, free of charge, to any person obtaining a copy
+#   of this software and associated documentation files (the "Software"), to deal
+#   in the Software without restriction, including without limitation the rights
+#   to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+#   copies of the Software, and to permit persons to whom the Software is
+#   furnished to do so, subject to the following conditions:
+#
+#   The above copyright notice and this permission notice shall be included in
+#   all copies or substantial portions of the Software.
+#
+#   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+#   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+#   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+#   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+#   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+#   SOFTWARE.
+# -----------------------------------------------------------------------------
 import pyxel
 
 # Initialize a 200×200 window
